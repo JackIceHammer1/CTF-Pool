5eacd99dc7a3731cbf7991409f0bf071c600f6b3
Jack <jackthor0608@gmail.com>
2024-07-25 14:26:05 -0400

    java

---
+++ KeyChain/KeyChain.java
@@ -0,0 +1,38 @@
+public class KeyChain {
+    public static void main(String[] args) throws Exception {
+        String key = "hkdasfdasfkvndndnknknksvnkannksvndnsvdnsvdskvndks2dsfadssf564dsf5a46dsf84dsaf546dsf54a4ds6f6sd/ds//fas/sfdsfsd23fa1dsf156as4546dsf4d4f6a8sfe/()()dfsjbhdlfhdishflsdafSD)(fdfKDSffdsfdsfodfsafkjo7684kjlfdkjfasklkjddksfadbkfdsbnjfkdlsjkjvbnkjlsvjdjavbldkjasbfkjlsfdsfds45f6d5sf4as6d5fd4s5fds6f15a6f4dsf54ds6af5d5s6f5as64fsf)s6f15a6f4dsf54ds6af5d5s6fs6f15a6f4dsf54ds6af5d5s6f";
+
+
+
+
+        //lbctf{thisistherightkey}
+
+
+
+        String key2 = "dafjdhlsfjhdsavanfsdinfvvoue8923nhvqevnuirenywrvnewn89r93n20vr983{}} hjdfhashfdfkdsjafl;dhfjhkjshfkjhhviuienabvsuidfjushf{}fjsafjpieojasfjdsifihesofhadjshafklhdusfueuihvnvoineasihfoidhjifjdosfoidjoasfdiof;}f[dfjdsfkhaeifvufnvevfoaufdnusofiueuiaofieuifusdfkjS7893275480IDFJUPufiKYFids;fidg]dfs{dfbkjgbkjjghjgdgdfgdfkjgdfi7578934578954___458y7984yth34t789y49t4uh9p0hfjdbhsliucovicuizvbvncxm,zuirtyhqjwrhohjighfidposhdfhsgpfjhndfiphhvuicpdgfgfd54gsdf56g";
+
+
+        String key3 = "dgfshjidgfshjdgfhjdghjijdgfgivnoivruvnyuryvnuiyeutvyretyv693v4nv783428y3478v34435v43485v34*534/5*v5v253453485v5345345y8nv{}oifoidu8970a70qb87r8979825r7396nbrtrwubrhth894nwb984tyb9rt7yr789ybntryne7tbryet79brtuyyt98y327t40b438ntyuytuirebhtritibunrety93unyt84yb43t1568t14481tr8tretb/r*trebtrethirutrytyrsdigufyngryiygonfygsouyngfydonryoirusgnrygrugr839bg6739brugnrgnyrby";
+        
+        
+        String key4 = "hfdashjidfs{dfbkjgbkjjghjgdgdfgdfkjgdfi7578934578954___458y7984yth34t789y49t4uh9p0hfjdbhsliucovicuizvbvncxm,zuirtyhqjwrhohjighfidposhdfhsgpfjhndfiphhvuicpdgfgfd54gsdf56g74f8dg7dfs68g4df8g7fg/*df/gf*g*d/fgdfg546dfgdfgdf45g8d6grgjrhisghisruiserhnovgrhuinruiegr768ewghi43hn34ty7843uihrkjghfdgsvnfhgdfsgfjdgkdhfsgjhdsfgudfgursdghjfdsguhfdjkgshfkjgdf}";
+
+
+        String key5 = "dusyfa7n7869w7n687v932t8b3976v78b63e78b78vg789gn78n7b34tb344t568344t34b9t81448bt734bt8*/asdfjdhsfuyn796945v79435v7n345v34vynrrvhnehnhnigdvnvn4vynurync9ewhwyvnwyv97hyntyv89t318v8t48917481v818v1t14*/44t3v98t34t3498t3434*53445obio9a0ii_)iojuii-0regrmetyu8yvt89y934ymdasfdasf67789h67896n9425nf35tvy834ntb94bt8834btreuthguhfugrgiburpiegupub0ue9gbrnuufugfdgdfgfdgfd";
+
+
+        String correctKey = "lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}lbctf{thisisalsotherightkey}";
+
+        int x = 2;
+        int y = 1;
+
+
+
+        String lock = key;
+
+        System.out.println("Key: lbctf{"+lock.substring(92,93)+lock.substring(93,94)+lock.substring(111,112)+lock.substring(111,112)+lock.substring(240,241)+lock.substring(92,93)+lock.substring(64,65)+lock.substring(52,53)+lock.substring(15, 16)+lock.substring(240,241)+lock.substring(58,59)+"}");
+
+
+
+    }
+}
