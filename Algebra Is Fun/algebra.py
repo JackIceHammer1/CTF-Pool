e7940e1cbfd22a0ee4bdecd1132fe75eed1699fb
Jack <jackthor0608@gmail.com>
2024-07-28 10:45:31 -0400

    programing

---
+++ Algebra Is Fun/algebra.py
@@ -0,0 +1,56 @@
+import random
+
+def jumble_string(input_string):
+    characters = list(input_string)
+    random.shuffle(characters)
+    jumbled_string = ''.join(characters)
+    return jumbled_string
+
+for i in range(1):
+    x = 12
+    if x < 30:
+        print("i am dumb")
+for i in range(1, 11):
+    print(i)
+
+
+
+
+print("Hi fellow friend")
+
+for i in range(3):
+    print("lbctf{9276iLOv257eev3786erythig}")
+    for i in range(1):
+        print("oops")
+        for i in range(5):
+            print("lbctf{iL0v3e27very2i769g3746}")
+            print("lbctf{iL0v3e26very2i729g3781}")
+            for i in range(1):
+                print("whoops")
+                for i in range(2):
+                    print("lbctf{eLe6O29i725ygvi7r873ev}")
+                    print("darn it")
+                    for i in range(1):
+                        for i in range(3):
+                            print("lbctf{iLOv257eev3786erythi9126g}")
+                            print("shucks")
+
+for i in range(1):
+    for i in range(1):
+        for i in range(1):
+            for i in range(1):
+                for i in range(1):
+                    for i in range(1):
+                        for i in range(1):
+                            for i in range(1):
+                                for i in range(1):
+                                    for i in range(1):
+                                        for i in range(1):
+                                            for i in range(1):
+                                                input_string = "lbctf{P34YthoN1IS32co435oL0487}"
+
+jumbled = jumble_string(input_string)
+
+print("Jumbled string:", jumbled)
+print("hmmm it looks encrypted to me")
+print("hint: i love math")
