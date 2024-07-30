40e84fbc7b8ac179f76548b37411a09158dbe8be
Jack <jackthor0608@gmail.com>
2024-07-30 14:20:16 -0400

    web

---
+++ Stopping the Script/Website/form.php
@@ -0,0 +1,4 @@
+<?php
+echo "<script>window.location.href='http://www.lbc2.org';</script>";
+echo "<p1>lbctf{r3d1r3cts_r_@nnoying!}</p1>";
+?>
---
+++ Stopping the Script/Website/index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>The Flag</title>
+  </head>
+  <body>
+    <h1>The flag is right here</h1>
+    <form action="form.php" method="post">
+      <input type="submit" value="click me for the flag">
+    </form>
+    <p1><br>just click the button<br>it will take you to the flag</p1>
+  <script src="form.js"></script>
+  </body>
+</html>
