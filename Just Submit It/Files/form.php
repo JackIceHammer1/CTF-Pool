83c906522791cbb55448547dc6ad1c5529a1a725
Jack <jackthor0608@gmail.com>
2024-07-30 22:34:49 -0400

    red team

---
+++ Just Submit It/Files/form.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_POST['button']))
+{
+  echo "<p1>lbctf{1_r@n_out_0f_f1@g_1d3@s}</p1>";
+}
+else
+{
+  echo "nope!";
+}
+?>
---
+++ Just Submit It/Files/index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <style type="style.css"></style>
+    <title>The Flag</title>
+  </head>
+  <body>
+    <h1>Enter the username and password</h1>
+    <form action="form.php" method="post">
+      <input type="text" placeholder="username"disabled>
+      <br>
+      <input type="password" placeholder="password"disabled>
+      <br>
+      <input type="submit" name="button" value="click me for the flag" disabled>
+    </form>
+    <p1><br>put in the username and password<br>then hit submit<br>no cheating!</p1>
+  </body>
+</html>
